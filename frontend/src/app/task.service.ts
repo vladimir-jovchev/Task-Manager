e70b116c9e042d9a347aee52a548996527d4afac
vladimir-jovchev <vladimir.jovchev@students.finki.ukim.mk>
2021-09-21 13:57:51 +0200

    WebReq service done

---
+++ frontend/src/app/task.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TaskService {
+
+  constructor() { }
+
+  createList(title: string){
+    //send web req to create new list
+    
+  }
+}
